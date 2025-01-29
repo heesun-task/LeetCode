6b23891ec5b22311306b976b0b73fb3340a8af97
Heesun Lee <hlee532@my.bcit.ca>
2025-01-29 10:27:13 -0800

    Time: 13 ms (87.04%), Space: 12.7 MB (52.84%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        # create dictionary to count number of chars
+        d= dict()
+        for char in s:
+            if char in d: 
+                d[char] += 1
+            else:
+                d[char] = 1
+
+        # remove chars in dict
+        for char in t:
+            if (char in d): 
+                d[char] -= 1
+                if d[char] == 0:
+                    del d[char]
+
+            else:
+                #if not exist, not anagram
+                return False
+
+        return len(d) == 0
+
+
+
+
+
+        
