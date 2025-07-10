0f88f8a5bac3115056b4c0455012080648f76939
Heesun Lee <hlee532@my.bcit.ca>
2025-07-10 14:50:59 -0700

    Time: 0 ms (100%), Space: 53.6 MB (63.98%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,20 +4,23 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    let left = 0
-    let right = nums.length - 1
+    // two pointers: left=0 right= nums.length - 1
+    let left = 0, right = nums.length - 1
 
+    // iterate until left > right
     while (left <= right) {
+        // find the mid Math.floor((left + right) / 2)
         const mid = Math.floor((left + right) / 2)
 
-        if (nums[mid] === target) 
-            return mid
-        else if (nums[mid] < target)
-            left = mid + 1
-        else
-            right = mid - 1
+        // if nums[mid] == target => return mid
+        if (nums[mid] === target) return mid 
+        // else if nums[mid] > target => right = mid -1 
+        else if (nums[mid] > target) right = mid - 1 
+        // else nums[mid] < target  => left = mid + 1
+        else left = mid + 1
     }
 
-    return left
+    //  return left pointer 
+    return left;
     
 };
