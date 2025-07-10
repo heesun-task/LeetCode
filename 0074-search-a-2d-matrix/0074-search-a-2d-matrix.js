6d4867c8fba28f0424887ec211845b0846a45dfd
Heesun Lee <hlee532@my.bcit.ca>
2025-07-10 15:39:34 -0700

    Time: 0 ms (100%), Space: 53.2 MB (77.21%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -3,26 +3,41 @@
  * @param {number} target
  * @return {boolean}
  */
+function findNumInMatrix(targetIdx, numOfColumns, matrix) {
+    let row, col; 
+    // find row :(targetIdx / numOfColumns) // e.g.,5 / 4 -> 1.xxx  => 2nd row (1 index row)
+    row = Math.floor(targetIdx / numOfColumns)
+
+    // find col :(targetIdx % numOfColumns) //e.g., 1 % 4 -> 1 => col index = 1 
+    col = (targetIdx % numOfColumns)
+
+    // find num -> matrix[row,col]
+    return matrix[row][col];
+} 
+
 var searchMatrix = function(matrix, target) {
+    // two pointers left = 0 and right (m x n - 1)
     let left = 0;
     let right = matrix.length * matrix[0].length - 1;
+    let numOfColumns = matrix[0].length;
 
-    while (left <= right) {
-        const mid = Math.floor((left + right) / 2);
-        const [midRow, midCol] = getPosition(mid, matrix[0].length);
-        const midNum = matrix[midRow][midCol];
+    // iterate this until left over right 
+    while (left <= right){
+        // find the mid number
+        let mid = Math.floor((left +  right) / 2)
+        let midNum = findNumInMatrix(mid, numOfColumns, matrix)
 
-        if (midNum === target)
-            return true
-        else if (midNum < target)
-            left = mid + 1;
-        else
-            right = mid - 1;
+        // midNum == target => return true
+        if (midNum === target) return true
+
+        // midNum > target => move right = mid - 1 
+        else if (midNum > target) right = mid - 1 
+
+        // midNum < target => move left = mid + 1
+        else if (midNum < target) left = mid + 1 
     }
-    return false;
-};
 
-const getPosition = (num,rowSize) => {
-    // return [rowIdx, colIdx]
-    return [Math.floor(num/rowSize), num % rowSize]
-}
+    // return false
+    return false
+    
+};
