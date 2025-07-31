1c0e6aa6253693776b4c6568a98a5ceb02482952
Heesun Lee <hlee532@my.bcit.ca>
2025-07-31 11:49:32 -0700

    Time: 0 ms (100%), Space: 53.8 MB (66.6%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -10,26 +10,35 @@
  * @param {number} n
  * @return {ListNode}
  */
- // Create a dummy node before the head to handle edge cases easily
- // Initialize two pointers: slow and fast at thedummy
- // Move the fast pointer n steps ahead
- // Then move both fast and slow one step at a time until fast reaches the end
- // At this point, slow is just before the node to be removed
- // Remove the nth node from the end by skipping slow.next
- // Finally, return dummy.next as the new head of the list
 var removeNthFromEnd = function(head, n) {
-    const dummy = new ListNode(0, head);
-    let slow = fast = dummy;
-    for (let i = 0; i < n; i++) {
+    // edgecase: if head.next ===null return null 
+    if (head.next === null) return null
+
+    const dummy = new ListNode(0, head)
+    // two pointers: 
+    let slow = fast = dummy
+
+    // move the fast pointer to the nth node from the first
+    for (let i =0; i < n; i++) {
         fast = fast.next;
     }
+    
+    // Note: gap b/w slow and fast will be exactly the n
 
-    while (fast.next !== null ) {
+    // loop while  fast is not null
+    while (fast !== null) {
+        // then if the next of fast node is null,  
+        if (fast.next === null) {
+            //Note: (means the next node of slow pointer is the node I need to remove)
+            // remove the next node of slow pointer
+            slow.next = slow.next.next;
+        }
+
+        // move slow and fast pointers together by one
         slow = slow.next;
         fast = fast.next;
     }
+    
+    return dummy.next;
 
-    slow.next = slow.next.next;
-
-    return dummy.next;    
 };
