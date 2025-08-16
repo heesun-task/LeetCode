ff15f9ee5c880c1d48f29c9c89595b634e33abfd
Heesun Lee <hlee532@my.bcit.ca>
2025-08-15 22:08:53 -0700

    Time: 4 ms (55.75%), Space: 60.8 MB (40.1%) - LeetHub

---
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ // [3]
+ // 
+var averageOfLevels = function(root) {
+    let queue = [root];
+    const avgs = []
+
+    // loop while queue is not empty
+    while (queue.length !== 0) {
+        const lvSize = queue.length;
+        let sum = 0;
+        const nextQueue = [];
+
+        // loop queue to lvSize
+        for (let i = 0; i < lvSize; i++){
+            const currNode = queue[i];
+            // add  val of current node to sum
+            sum += currNode.val;
+            
+            // add children of current node to queue if exist
+            if(currNode.left) nextQueue.push(currNode.left);
+            if(currNode.right) nextQueue.push(currNode.right);
+        }
+
+        // avg = divide sum by level Size
+        const avg = sum / lvSize;
+
+        // push avg to avgs
+        avgs.push(avg);
+
+        queue = nextQueue;
+    }
+    
+    return avgs;
+};
