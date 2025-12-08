a90b62b212febf3285bbd5428852ad9484b4a227
Heesun Lee <heesun.task@gmail.com>
2025-12-07 19:20:21 -0800

    Time: 24 ms (30.77%), Space: 55.3 MB (48.08%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+var countTriples = function(n) {
+    if (n < 5) return 0;
+    const sq = new Set(Array.from({length:n}, (_, i) => (i+1) ** 2)); 
+    let cnt = 0;
+
+    for (let i = 1; i < n; i++) {
+        for (let j = i+1; j < n; j++) {
+            // If a^2 + b^2 is a perfect square c^2 (where c <= n),
+            // then both (a, b, c) and (b, a, c) count as valid triples.
+            if (sq.has(i**2 + j**2)) cnt+=2; 
+        }
+    }
+    return cnt;
+};
