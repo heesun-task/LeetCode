fe1be7cb4949836c571e037d436f38c1f2a787e2
Heesun Lee <heesun.task@gmail.com>
2026-03-02 20:32:36 -0800

    Time: 18 ms (58.59%), Space: 74.4 MB (40.36%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -2,12 +2,27 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
-    const set = new Set();
+ /*
+    Goal: return true if duplicated #, false if no duplicated number
 
-    return nums.some(cur => {
-        if (set.has(cur)) return true;
-        set.add(cur);
-        return false;
-    });
+    Plan:
+    - create a Set
+    - loop num in nums
+        - num exists in the Set -> return true
+        - not exsits ->  add the num in the Set, continue to the next loop
+    - return false 
+
+    space complexity: O(n)
+    time complexity: O(n)
+  */
+var containsDuplicate = function(nums) {
+    const seen = new Set();
+
+    for(const num of nums) {
+        if (seen.has(num)) return true;
+        else {
+            seen.add(num);
+        }
+    }
+    return false; 
 };
